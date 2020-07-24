f792b7d6ffb0a3275be6fd6b5ed86ee401723f61
ErnestoGtz <ernesto.gtz@itcoach.com.mx>
2020-07-23 20:09:29 -0500

    end of app

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React,{useState,useContext} from 'react'
+import { UserContext } from './UserContext';
+
 
 export const LoginScreen = () => {
+    
+    const { setUser } = useContext(UserContext);
+    
     return (
         <div>
             <h1>LoginScreen</h1>
             <hr />
+            <button 
+                className="btn btn-secondary"
+                onClick={() => setUser({
+                    id:'04131024',
+                    name:'Ernesto Gtz'
+                })}
+            >
+                Login
+            </button>
         </div>
     )
 }
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
+    
+    
+    const [user, setUser] = useState({});
+    
+    // const user = {
+    //     id:1234,
+    //     name:'Ernesto',
+    //     email:'ernesto.gtz@itcoach.com.mx'
+    // }
+    
+    
     return (
-        <div>
-            <h1>MainApp</h1>
-            <hr />
-        </div>
+        <UserContext.Provider value={{
+            user,
+            setUser
+
+        }}>
+            <AppRouter />
+        </UserContext.Provider>
+        
+        
     )
 }
